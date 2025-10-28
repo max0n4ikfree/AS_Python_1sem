239ada5e758d0fe9ab06f9eaa4d03b8f52ffa8d2
Максим Мыцу <max0n4ik.free@gmail.com>
2025-10-28 14:29:48 +0300

    Update main.py

--- 1.3lab/main.py
+++ 1.3lab/main.py
@@ -1,2 +1,11 @@
-if __name__ == "__main__":
-    pass
+n = int(input("Введите натуральное число n: "))
+
+total_sum = 0
+factorial = 1
+
+for i in range(1, n + 1):
+    factorial *= i
+    total_sum += 1 / factorial
+
+# Вывод результата
+print(f"Сумма ряда: {total_sum:.10f}")
