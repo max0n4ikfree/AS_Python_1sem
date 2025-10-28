8d0cfc03f10ced968cec667ebea384cec92eb50b
Максим Мыцу <max0n4ik.free@gmail.com>
2025-10-28 14:14:18 +0300

    Update main.py

--- 1.2lab/main.py
+++ 1.2lab/main.py
@@ -1,2 +1,7 @@
-if __name__ == "__main__":
-    pass
+price_per_kg = float(input("Введите цену за 1 кг конфет: "))
+
+print("Вес (кг) | Стоимость")
+print("---------|-----------")
+for weight in [1.2, 1.4, 1.6, 1.8, 2.0]:
+    cost = price_per_kg * weight
+    print(f"{weight:6.1f}  | {cost:8.2f}")
