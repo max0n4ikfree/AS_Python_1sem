f576250d13b979046eb22684b8310017ffe0e825
Максим Мыцу <max0n4ik.free@gmail.com>
2025-10-28 14:34:11 +0300

    Update main.py

--- 1.5lab/main.py
+++ 1.5lab/main.py
@@ -1,2 +1,7 @@
-if __name__ == "__main__":
-    pass
+numbers = list(map(int, input("Введите числа через пробел: ").split()))
+
+print("Исходный список:", numbers)
+
+result = sorted([x for x in numbers if x % 3 == 0])
+
+print("Числа, кратные 3, упорядоченные по возрастанию:", result)
