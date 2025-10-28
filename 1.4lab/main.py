1650593e720128d911053b4a2943c55421db297b
Максим Мыцу <max0n4ik.free@gmail.com>
2025-10-28 14:32:25 +0300

    Update main.py

--- 1.4lab/main.py
+++ 1.4lab/main.py
@@ -1,2 +1,7 @@
-if __name__ == "__main__":
-    pass
+numbers = list(map(int, input("Введите числа через пробел: ").split()))
+
+print("Исходный список:", numbers)
+
+result = [0 if x % 2 != 0 else x for x in numbers]
+
+print("Результат:", result)
