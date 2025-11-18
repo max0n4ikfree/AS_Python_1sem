ccb27da1dd19ee4d18f09af4a2b9654ee3cc0763
Максим Мыцу <max0n4ik.free@gmail.com>
2025-11-18 12:07:56 +0300

    Update main.py

--- 2.1lab/main.py
+++ 2.1lab/main.py
@@ -1,2 +1,25 @@
-if __name__ == "__main__":
-    pass
+print("а) Введите числа через пробел:")
+chisla = input().split()
+summa = 0
+for x in chisla:
+    summa += int(x)
+print("Сумма:", summa)
+
+# б) Список в словарь
+print("\nб) Введите слова через пробел:")
+slova = input().split()
+slovar = {}
+for x in slova:
+    slovar[x] = {}
+print("Словарь:", slovar)
+
+# в) Проверка одинаковых значений
+print("\nв) Введите числа через пробел:")
+chisla2 = input().split()
+odinakovie = True
+pervoe = chisla2[0]
+for x in chisla2:
+    if x != pervoe:
+        odinakovie = False
+        break
+print("Все одинаковые:", odinakovie)
