7e1899d06e50fe97c080c2a15b39470bdf5b1f20
Максим Мыцу <max0n4ik.free@gmail.com>
2025-11-18 12:18:43 +0300

    Update main.py

--- 2.7lab/main.py
+++ 2.7lab/main.py
@@ -1,2 +1,16 @@
-if __name__ == "__main__":
-    pass
+with open('file.txt', 'r', encoding='utf-8') as file:
+    lines = file.readlines()
+
+# Считаем абзацы
+paragraphs = 0
+prev_line_empty = True  # считаем, что перед первым абзацем пустая строка
+
+for line in lines:
+    if line.strip() == '':  # если строка пустая
+        prev_line_empty = True
+    else:
+        if prev_line_empty:  # если предыдущая строка была пустой, значит это новый абзац
+            paragraphs += 1
+        prev_line_empty = False
+
+print(f"Количество абзацев: {paragraphs}")
