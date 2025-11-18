e830b081c97c8444cefa47a0b12a3a26cd564124
Максим Мыцу <max0n4ik.free@gmail.com>
2025-11-18 12:02:26 +0300

    Update main.py

--- 2.10lab/main.py
+++ 2.10lab/main.py
@@ -1,2 +1,19 @@
-if __name__ == "__main__":
-    pass
+height = float(input("Введите рост в см: "))
+if height < 0 or height > 300:
+    print("Некорректный рост")
+else:
+    print("Рост введен корректно")
+
+# б) Проверка веса
+weight = float(input("Введите вес в кг: "))
+if weight< 0 or weight > 500:
+    print("Некорректный вес")
+else:
+    print("Вес введен корректно")
+
+# в) Проверка температуры
+temperature = float(input("Введите температуру в градусах: "))
+if temperature < -273.15:
+    print("Некорректная температура")
+else:
+    print("Температура введена корректно")
