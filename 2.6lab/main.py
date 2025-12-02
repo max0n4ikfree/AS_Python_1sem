8736bb523e140169e3d57af1285e87f847e20b97
Максим Мыцу <max0n4ik.free@gmail.com>
2025-12-02 12:00:26 +0300

    Update main.py

--- 2.6lab/main.py
+++ 2.6lab/main.py
@@ -1,2 +1,16 @@
-if __name__ == "__main__":
-    pass
+def modulus(z):
+    return (z.real**2 + z.imag**2)**0.5
+
+def multiply(z1, z2):
+    return z1 * z2
+
+def divide(z1, z2):
+    return z1 / z2
+    import cmath
+
+z1 = complex(3, 4)
+z2 = complex(1, 2)
+
+print(modulus(z1))
+print(multiply(z1, z2))
+print(divide(z1, z2))
